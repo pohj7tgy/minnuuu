3b01c738bb9ab69f44052ae01a2d867b9a4393cb
pohj7tgy <prasadgaikwad152@gmail.com>
2026-02-09 01:12:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,11 @@ function hearts() {
     }, 3000);
   }, 200);
 }
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}, 3000);
